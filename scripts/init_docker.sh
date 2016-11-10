6c35b047b09418286ff6af6a80ae8758bae2fe7d
John Robinson <john.holland.robinson@gmail.com>
2016-11-10 02:22:07 -0600

    Add cache busting for base image

--- scripts/init_docker.sh
+++ scripts/init_docker.sh
@@ -2,20 +2,26 @@
 MAX_CACHE_AGE=604800 # seconds
 DOCKER_REPO=${DOCKER_REPO:-postgrest}
 
-if [[ -e ~/.docker/image.tar ]]; then 
-	docker load --input ~/.docker/image.tar; 
-	docker images -a --digests;
+if [[ -e ~/.docker/image.tar ]]; then
+  let "AGE=$(date +%s) - $(date --date=$(docker inspect -f={{.Created}} ${DOCKER_REPO}dev) +%s)";
+
+  echo "${AGE} < ${MAX_CACHE_AGE}";
+  if [[ $AGE -lt $MAX_CACHE_AGE ]]; then
+    echo "Loading image from tar..";
+    docker load --input ~/.docker/image.tar;
+    docker images -a --digests;
+  else
+    echo "Cache invalidated. Deleting image... ";
+    rm -rf ~/.docker;
+  fi
 fi
 
-let "AGE=$(date --date=$(docker inspect -f={{.Created}} ${DOCKER_REPO}dev) +%s)";
-if [[ $MAX_CACHE_AGE -lt $AGE ]]; then
-	rm -rf ~/.docker;
-fi
+if [[ ! -e ~/.docker/image.tar ]]; then
+  echo "Building image..."
+  docker-compose build development;
+  docker images -a --digests;
 
-if [[ ! -e ~/.docker/image.tar ]]; then 
-	docker-compose build development; 
-	docker images -a --digests;
-
-	mkdir -p ~/.docker; 
-	docker save ${DOCKER_REPO}dev > ~/.docker/image.tar;
+  mkdir -p ~/.docker;
+  echo "Saving image..."
+  docker save ${DOCKER_REPO}dev > ~/.docker/image.tar;
 fi
